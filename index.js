b61d705836b12e9e29d56226bed3835533452d17
Luciano <lucianocisneros22@gmail.com>
2021-03-01 22:11:51 -0300

    Se agrego html del proyecto y se agrego funcion conm alert

---
+++ index.js
@@ -0,0 +1,33 @@
+function informacion (){
+    let info = prompt("Nos gustaria saber donde nos conociste:")
+    return alert("Gracias por tu colaboracion!")
+}
+
+informacion();
+
+//for(let i = 0; i <= 7; i++){
+    ///if(i != 7){
+        //alert('Hoy no es el dia numero 7!');
+    //} else {
+       // alert('Hoy es el dia numero 7!');
+   // }
+//}
+
+//function ingresarPrecio (){
+//    let precioCosto = prompt("Ingresa el costo del producto:");
+ //   return precioCosto;
+//}
+
+//function sumarIva (){
+ //   let iva = precioCosto * 0.21;
+//    return iva;
+//}
+
+//function precioIva (){
+//    let resultado = precioCosto + iva;
+ //   return alert('El precio + iva = ' + resultado);
+//}
+
+//ingresarPrecio();
+//sumarIva();
+//precioIva();
