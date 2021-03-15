5cbad7c106a197165bd8d3eff4d59e54149e9933
Luciano <lucianocisneros22@gmail.com>
2021-03-15 15:37:00 -0300

    correccion realizada en los nombres de las variables

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ class turno {
     }
 
     nuevo_turno = new turno (document.querySelector("input[name = 'nombre']").value, document.querySelector("input[name = 'apellido']").value, document.querySelector("input[name = 'email']").value, document.querySelector("input[name = 'telefono']").value, document.querySelector("input[name = 'fecha']").value, servicios = document.querySelector("select[name = 'servicios']").value, document.querySelector("textarea[name = 'motivo']").value);
-    almacenar_turno = pacientes.push(this.nuevo_turno);
+    almacenar_turno = turnos.push(this.nuevo_turno);
 
 }
