31df6089b6d4872677422efd32387efd06299289
Luciano <lucianocisneros22@gmail.com>
2021-03-27 12:29:33 -0300

    Se logro almacenar un turno en el array, pero queda solucionar que se guarden los datos enviados

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function nombre (){
     let nombre_pantalla = document.getElementById('nombre_usuario');
 
     nombre_pantalla.innerHTML = pedir_nombre;
+
+    sessionStorage.setItem('Nombre', pedir_nombre);
     
     return nombre_pantalla;
 
@@ -27,10 +29,13 @@ class turno {
          this.motivo = motivo;
     }
 
-    nuevo_turno = new turno (document.querySelector("input[name = 'nombre']").value, document.querySelector("input[name = 'email']").value, document.querySelector("input[name = 'telefono']").value, document.querySelector("input[name = 'fecha']").value, servicios = document.querySelector("select[name = 'servicios']").value, document.querySelector("textarea[name = 'motivo']").value);
-    almacenar_turno = turnos.push(this.nuevo_turno);
-
 }
+
+nuevo_turno = new turno (document.querySelector("input[name = 'nombre']").value, document.querySelector("input[name = 'email']").value, document.querySelector("input[name = 'telefono']").value, document.querySelector("input[name = 'fecha']").value, servicios = document.querySelector("select[name = 'servicios']").value, document.querySelector("textarea[name = 'motivo']").value);
+
+turnos.push(nuevo_turno);
+
+console.log(turnos);
 
 function enviar_form(evento) {
     if (evento.keyCode == 13){
