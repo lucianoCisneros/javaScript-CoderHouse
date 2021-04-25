a3edb4b7b9b0ed716143dae5042688a72a2d874d
Luciano <lucianocisneros22@gmail.com>
2021-04-25 12:08:28 -0300

    Se corrigio el funcionamiento del almacenado de turnos y se agrego un alert

--- javascript/turnos.js
+++ javascript/turnos.js
@@ -1,22 +1,22 @@
 $( document ).ready(function (){
+
     let pedir_nombre = prompt('Nos gustaria saber tu nombre:');
     let nombre_pantalla = document.getElementById('nombre_usuario');
-
-    nombre_pantalla.innerHTML = pedir_nombre;
-
     sessionStorage.nombre = pedir_nombre;
+
+    return nombre_pantalla.innerHTML = pedir_nombre;
     
 })
 
 
 class turno {
 
-    constructor(nombre, apellido, email, telefono, fecha, servicios, prepaga, motivo){
+    constructor(nombre, email, telefono, fecha, horario, servicios, prepaga, motivo){
          this.nombre = nombre;
-         this.apellido = apellido;
          this.email = email;
          this.telefono = telefono;
          this.fecha = fecha;
+         this.horario = horario;
          this.servicios = servicios;
          this.prepaga = prepaga;
          this.motivo = motivo;
@@ -24,15 +24,22 @@ class turno {
 
 }
 
-nuevo_turno = new turno (document.querySelector('input[name = "nombre"]').value, document.querySelector('input[name = "email"]').value, document.querySelector('input[name = "telefono"]').value, document.querySelector('input[name = "fecha"]').value, servicios = document.querySelector('select[name = "servicios"]').value, document.querySelector('select[name = "prepaga"]').value, document.querySelector('textarea[name = "motivo"]').value);
-turno_JSON = JSON.stringify(nuevo_turno);
-localStorage.turno = turno_JSON;
+function espera(){
+ 
+    nuevo_turno = new turno (document.getElementById('nombre').value, document.querySelector('input[name = "email"]').value, document.querySelector('input[name = "telefono"]').value, document.querySelector('input[name = "fecha"]').value, document.querySelector('select[name = "horario"]').value, document.querySelector('select[name = "servicios"]').value, document.querySelector('select[name = "prepaga"]').value, document.querySelector('textarea[name = "motivo"]').value);
+    turno_JSON = JSON.stringify(nuevo_turno);
+    localStorage.turno = turno_JSON;
+    alert('Gracias ' + sessionStorage.nombre + ' te esperamos el dia ' + document.querySelector('input[name = "fecha"]').value + ' en el siguiente horario: ' + document.querySelector('select[name = "horario"]').value + '.');
+   
+}
 
 
 $( '#motivo' ).keypress((evento) => {
+
     if (evento.keyCode == 13){
         
-        alert('Gracias por enviar el formulario!');
+        alertify.alert('Globalite', 'Gracias por enviar el formulario!');
 
     }
+
 })
